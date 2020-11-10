63becd3d4a75fc95a1d7f1078504b244525d133a
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-10 12:58:22 +0100

    feat: persist session

--- src/store/store.ts
+++ src/store/store.ts
@@ -6,11 +6,21 @@ const rootReducer = combineReducers({
   auth: authReducer,
 })
 
+const auth = localStorage.getItem('auth')
+  ? JSON.parse(localStorage.getItem('auth') as string)
+  : undefined
+
 const store = configureStore({
   reducer: rootReducer,
+  preloadedState: { auth },
+})
+
+store.subscribe(() => {
+  const auth = store.getState().auth
+  localStorage.setItem('auth', JSON.stringify(auth))
 })
 
 export default store
 
-export type IStoreState = ReturnType<typeof rootReducer>
 export type IStoreDispatch = typeof store.dispatch
+export type IStoreState = ReturnType<typeof rootReducer>
