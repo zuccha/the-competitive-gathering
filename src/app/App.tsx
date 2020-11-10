c7e3f7d6f7a17f5ae887d43537f91a8779745824
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-10 12:47:08 +0100

    feat: limit app width

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,20 +4,23 @@ import Navbar from './components/Navbar'
 import RoutePrivate from './components/RoutePrivate'
 import RoutePublic from './components/RoutePublic'
 import Login from './views/Login'
+import styles from './App.module.css'
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <RoutePublic path='/login'>
-          <Login />
-        </RoutePublic>
-        <RoutePrivate path='/'>
-          OVERVIEW
-        </RoutePrivate>
-      </Switch>
-    </BrowserRouter>
+    <div className={styles['app']}>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <RoutePublic path='/login'>
+            <Login />
+          </RoutePublic>
+          <RoutePrivate path='/'>
+            OVERVIEW
+          </RoutePrivate>
+        </Switch>
+      </BrowserRouter>
+    </div>
   )
 }
 
