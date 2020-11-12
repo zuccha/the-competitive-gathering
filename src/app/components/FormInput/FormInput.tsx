b43bf6213239094fa35670a698b93b6e8763f3ca
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-12 01:58:31 +0100

    feat: add label prop to form input component

--- src/app/components/FormInput/FormInput.tsx
+++ src/app/components/FormInput/FormInput.tsx
@@ -4,21 +4,31 @@ import { ErrorMessage, Field } from 'formik'
 import styles from './FormInput.module.css'
 
 interface IFormInputProps {
-  className?: string,
-  name: string,
-  placeholder?: string,
-  type?: string,
+  className?: string
+  name: string
+  label?: string
+  placeholder?: string
+  type?: string
+  leaveSpaceForError?: boolean
 }
 
 const FormInput: React.FC<IFormInputProps> = ({
   className,
   name,
+  label,
   placeholder,
   type,
+  leaveSpaceForError,
 }) => (
   <div className={classnames(styles['form-input'], className)}>
+    {label && <div className={styles['form-input-label']}>{label}</div>}
     <Field name={name} placeholder={placeholder} type={type} />
-    <div className={styles['form-input-error-message']}>
+    <div
+      className={classnames(
+        styles['form-input-error-message'],
+        leaveSpaceForError && styles['form-input-error-message-always-spaced'],
+      )}
+    >
       <ErrorMessage name={name} />
     </div>
   </div>
