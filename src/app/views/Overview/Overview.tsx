00510e3e7521836d14b9cb850d429a7120c6d3fc
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-12 12:23:58 +0100

    feat: add button for creating a league

--- src/app/views/Overview/Overview.tsx
+++ src/app/views/Overview/Overview.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Button from '../../components/Button'
 import Leagues from './Leagues'
 import Standings from './Standings'
+import styles from './Overview.module.css'
 
 const Overview: React.FC = () => {
   return (
     <div>
-      <h1>MTG Leagues</h1>
+      <h1>The Competitive Gathering</h1>
+      <div className={styles['overview-buttons']}>
+        <Button>Create league</Button>
+      </div>
       <Standings />
       <Leagues />
     </div>
