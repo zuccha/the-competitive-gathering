6230032ed10804be5dcd5491d599785fa5304985
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-15 15:23:04 +0100

    feature(be): generate matches when starting league and do not end league if not all matches are resolved

--- backend/api/models/League.py
+++ backend/api/models/League.py
@@ -1,7 +1,7 @@
+from datetime import date
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
-from django.db.models import CASCADE
 
 
 class League(models.Model):
@@ -33,6 +33,20 @@ class League(models.Model):
   players_min = models.PositiveIntegerField(validators=[MinValueValidator(2)], default=2)
   players_max = models.PositiveIntegerField(validators=[MinValueValidator(2)], blank=True, null=True)
   rounds = models.PositiveIntegerField(validators=[MinValueValidator(1)], default=1)
+
+  def start(self):
+    self.status = League.Status.ONGOING
+    self.date_start = date.today()
+    self.save()
+
+  def cancel(self):
+    self.status = League.Status.CANCELED
+    self.save()
+
+  def done(self):
+    self.status = League.Status.DONE
+    self.date_end = date.today()
+    self.save()
 
   def __str__(self):
     return str(self.id) + " (" + self.status + ")"
--- backend/api/views/LeagueStatus.py
+++ backend/api/views/LeagueStatus.py
@@ -1,8 +1,24 @@
 import json
-from datetime import date
 from django.http import HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
 from rest_framework.views import APIView
 from ..models.League import League as LeagueModel
+from ..models.Match import Match as MatchModel
+
+def generate_matches(league):
+  players = league.players.all()
+  for round in range(league.rounds):
+    for i in range(0, len(players)):
+      for j in range(i + 1, len(players)):
+        player1, player2 = (players[i], players[j]) if round % 2 == 0 else (players[j], players[i])
+        match = MatchModel()
+        match.league = league
+        match.player1 = player1
+        match.player2 = player2
+        match.save()
+
+def is_league_ready(league):
+  matches = MatchModel.objects.filter(league=league).all()
+  return all(match.status == MatchModel.Status.DONE for match in matches)
 
 class LeagueStatus(APIView):
   def put(self, request, id):
@@ -25,24 +41,20 @@ class LeagueStatus(APIView):
       if league.players_max != None and league.players.count() > league.players_max:
         return HttpResponseBadRequest('cannot start a league if the number of registered players exceedes the maximum')
 
-      # TODO: generate matches.
-
-      league.status = LeagueModel.Status.ONGOING
-      league.date_start = date.today()
-      league.save()
+      league.start()
+      generate_matches(league)
 
       return JsonResponse(league.to_json())
 
     if league.status == LeagueModel.Status.ONGOING and status == LeagueModel.Status.CANCELED:
-      league.status = LeagueModel.Status.CANCELED
-      league.save()
+      league.cancel()
       return JsonResponse(league.to_json())
 
     if league.status == LeagueModel.Status.ONGOING and status == LeagueModel.Status.DONE:
-      # TODO: check that all matches are resolved.
-      league.status = LeagueModel.Status.DONE
-      league.date_end = date.today()
-      league.save()
+      if not is_league_ready(league):
+        return HttpResponseBadRequest('cannot end a league if there still are matches to play')
+
+      league.done()
       return JsonResponse(league.to_json())
 
     return HttpResponseBadRequest('league cannot transition from `' + league.status + '` to `' + status + '`')
