5270a259858288d0fbaf87329d830e9c64d4ecff
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-15 12:52:41 +0100

    feat(be): partially implement status update for leagues and removal

--- backend/api/views/Leagues.py
+++ backend/api/views/Leagues.py
@@ -1,6 +1,7 @@
 import json
+from datetime import date
 from django.contrib.auth.models import User
-from django.http import HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
 from rest_framework.views import APIView
 from ..models.League import League
 
@@ -64,3 +65,61 @@ class Leagues(APIView):
     league.players.set(players)
 
     return JsonResponse(league.to_json())
+
+  def put(self, request, id):
+    if id == '':
+      return HttpResponseBadRequest('required parameter `id` is missing')
+
+    if request.data == None or request.data.get('status') == None:
+      return HttpResponseBadRequest('required field `status` is missing')
+
+    status = request.data.get('status')
+    league = League.objects.filter(id=id).first()
+
+    if league == None:
+      return HttpResponseNotFound('league not found')
+
+    if league.status == League.Status.PENDING and status == League.Status.ONGOING:
+      if league.players.count() < league.players_min:
+        return HttpResponseBadRequest('cannot start a league if the minimum number of players is not met')
+
+      if league.players_max != None and league.players.count() > league.players_max:
+        return HttpResponseBadRequest('cannot start a league if the number of registered players exceedes the maximum')
+
+      # TODO: generate matches.
+
+      league.status = League.Status.ONGOING
+      league.date_start = date.today()
+      league.save()
+
+      return JsonResponse(league.to_json())
+
+    if league.status == League.Status.ONGOING and status == League.Status.CANCELED:
+      league.status = League.Status.CANCELED
+      league.save()
+      return JsonResponse(league.to_json())
+
+    if league.status == League.Status.ONGOING and status == League.Status.DONE:
+      # TODO: check that all matches are resolved.
+      league.status = League.Status.DONE
+      league.date_end = date.today()
+      league.save()
+      return JsonResponse(league.to_json())
+
+    return HttpResponseBadRequest('league cannot transition from `' + league.status + '` to `' + status + '`')
+
+
+  def delete(self, request, id):
+    if id == '':
+      return HttpResponseBadRequest('required parameter `id` is missing')
+
+    league = League.objects.filter(id=id).first()
+
+    if league == None:
+      return HttpResponseNotFound('league not found')
+
+    if league.status != League.Status.PENDING:
+      return HttpResponseBadRequest('cannot cancel a league that is not pending')
+
+    league.delete()
+    return HttpResponse()
