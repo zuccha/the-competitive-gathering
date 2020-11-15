7509779111acd3c27843c3d20141618b8238a3b4
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-15 12:15:34 +0100

    feat(fe): create league via backend

--- frontend/src/store/slices/leagues/thunks/createLeague/createLeague.ts
+++ frontend/src/store/slices/leagues/thunks/createLeague/createLeague.ts
@@ -1,8 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import ApiLeague from '../../../../../types/ApiLeague'
 import { IApiLeagueInput } from '../../../../../types/ApiLeagueInput'
-import ErrorHttp from '../../../../../types/ErrorHttp'
 import { ILeague } from '../../../../../types/League'
-import wait from '../../../../../utils/wait'
+import withErrorHttp from '../../../../../utils/withErrorHttp'
+import api from '../../../../api'
 import { IStoreDispatch, IStoreState } from '../../../../store'
 
 const createLeague = createAsyncThunk<
@@ -11,31 +12,10 @@ const createLeague = createAsyncThunk<
   { state: IStoreState, dispatch: IStoreDispatch }
 >(
   'leagues/createLeague',
-  async apiLeagueInput => {
-    // TODO: Implement once server is ready.
-    await wait(500)
-    if (apiLeagueInput.format === 'MODERN') {
-      throw new ErrorHttp('400')
-    }
-    if (apiLeagueInput.format === 'LEGACY') {
-      throw new ErrorHttp('403')
-    }
-    if (apiLeagueInput.format === 'VINTAGE') {
-      throw new ErrorHttp('500')
-    }
-    return {
-      id: '1',
-      creator: apiLeagueInput.creator,
-      players: apiLeagueInput.players,
-      status: 'PENDING',
-      format: apiLeagueInput.format,
-      dateStart: undefined,
-      dateEnd: undefined,
-      playersMin: apiLeagueInput.players_min,
-      playersMax: apiLeagueInput.players_max,
-      rounds: apiLeagueInput.rounds,
-    }
-  },
+  withErrorHttp(async apiLeagueInput => {
+    const { data } = await api.post('/leagues/', apiLeagueInput)
+    return ApiLeague.toLeague(data)
+  }),
 )
 
 export default createLeague
