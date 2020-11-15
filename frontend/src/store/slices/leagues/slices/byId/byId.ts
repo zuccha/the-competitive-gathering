7a289b7168311941aef634ab2fb5509dfbb22859
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-15 10:05:06 +0100

    fix(fe): actually update store after fetching a league by id

--- frontend/src/store/slices/leagues/slices/byId/byId.ts
+++ frontend/src/store/slices/leagues/slices/byId/byId.ts
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { ILeague } from '../../../../../types/League'
 import Request, { IRequest } from '../../../../../types/Request'
+import fetchLeagueById from '../../thunks/fetchLeagueById'
 import fetchLeagues from '../../thunks/fetchLeagues'
 
 const byId = createSlice({
@@ -8,6 +9,15 @@ const byId = createSlice({
   initialState: {} as Record<string, IRequest<ILeague>>,
   reducers: {},
   extraReducers: builder => {
+    builder.addCase(fetchLeagueById.pending, (state, action) => {
+      state[action.meta.arg] = Request.makeLoading()
+    })
+    builder.addCase(fetchLeagueById.fulfilled, (state, action) => {
+      state[action.meta.arg] = Request.makeSuccess(action.payload)
+    })
+    builder.addCase(fetchLeagueById.rejected, (state, action) => {
+      state[action.meta.arg] = Request.makeFailure()
+    })
     builder.addCase(fetchLeagues.fulfilled, (state, action) => {
       action.payload.forEach(league => {
         state[league.id] = Request.makeSuccess(league)
