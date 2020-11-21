0bf08f278615e72feafafba8d5965167ab38c766
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-21 14:28:54 +0100

    build(be): setup postresql db for heroku

--- trg/settings.py
+++ trg/settings.py
@@ -86,14 +86,14 @@ STATICFILES_DIRS = [
 
 WSGI_APPLICATION = 'trg.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
   'default': {
-    'ENGINE': 'django.db.backends.sqlite3',
-    'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'trg',
+    'USER': 'name',
+    'PASSWORD': '',
+    'HOST': 'localhost',
+    'PORT': '',
   }
 }
 
@@ -148,3 +148,8 @@ REST_FRAMEWORK = {
     'rest_framework.authentication.TokenAuthentication',
   ],
 }
+
+try:
+  from .local_settings import *
+except ImportError:
+  pass
