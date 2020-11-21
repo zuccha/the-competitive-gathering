399201e1389cf18833c859770b2c701215100416
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2020-11-21 18:06:38 +0100

    feat(fe): log REACT_APP_HOST for debugging purposes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -9,6 +9,7 @@ import Login from './views/Login'
 import Overview from './views/Overview'
 
 const App: React.FC = () => {
+  console.log('REACT_APP_HOST:', process.env.REACT_APP_HOST)
   return (
     <div className={styles['app']}>
       <BrowserRouter>
